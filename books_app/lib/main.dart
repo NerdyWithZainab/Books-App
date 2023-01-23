294c24eaaec1937dd7c968bf8e5c899f713ddede
Zainab Rangwala <71223968+zfr18@users.noreply.github.com>
2023-01-23 16:20:31 +0400

    Edited the main.dart file

--- books_app/lib/main.dart
+++ books_app/lib/main.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void main() {
+    runApp(const MyApp());
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
