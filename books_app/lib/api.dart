95506c4bfd92e6acbe9a8d2ab8100216978cf954
Zainab Rangwala <71223968+zfr18@users.noreply.github.com>
2023-01-23 17:05:04 +0400

    Made some edits

--- books_app/lib/api.dart
+++ books_app/lib/api.dart
@@ -3,17 +3,15 @@ import 'package:http/http.dart' as http;
 import 'books.dart';
 import 'package:books_app/config.dart';
 
-class BooksApi {
-  Future<List<Book>?> fetchBooks() async {
-    final response = await http.get(Uri.parse(
-        'https://www.googleapis.com/books/v1/volumes?q=*&key=$API_KEY"'));
+Future<List<Book>?> fetchBooks() async {
+  final response = await http.get(Uri.parse(
+      'https://www.googleapis.com/books/v1/volumes?q=*&key=$API_KEY"'));
 
-    if (response.statusCode == 200) {
-      // If the call to the API was successful, parse the JSON
-      List<dynamic> jsonResponse = jsonDecode(response.body)['items'];
-      return (jsonResponse.map((book) => Book.fromJson(book)).toList());
-    } else {
-      throw Exception('Failed to load books');
-    }
+  if (response.statusCode == 200) {
+    // If the call to the API was successful, parse the JSON
+    List<dynamic> jsonResponse = jsonDecode(response.body)['items'];
+    return (jsonResponse.map((book) => Book.fromJson(book)).toList());
+  } else {
+    throw Exception('Failed to load books');
   }
 }
--- books_app/lib/main.dart
+++ books_app/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() {
+  runApp(const MyApp());
+}
 
-  void main() {
-    runApp(const MyApp());
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
