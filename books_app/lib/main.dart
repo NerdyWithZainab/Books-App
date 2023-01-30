a442050bd41b40de5de500c6eade3ce0290ca30d
Zainab Rangwala <71223968+zfr18@users.noreply.github.com>
2023-01-30 16:13:09 +0400

    Created different layouts

--- books_app/lib/main.dart
+++ books_app/lib/main.dart
@@ -1,41 +1,11 @@
 import 'package:books_app/api.dart';
+import 'package:books_app/responsive/mobile_screen_layout.dart';
+import 'package:books_app/responsive/responsive_layout_screen.dart';
+import 'package:books_app/responsive/web_screen_layout.dart';
 import 'package:flutter/material.dart';
-import 'books.dart';
 
 final booksAPI = BooksApi();
 void main() => runApp(const MyApp());
-
-class DisplayBooks extends StatefulWidget {
-  const DisplayBooks({super.key});
-
-  @override
-  State<DisplayBooks> createState() => _DisplayBooksState();
-}
-
-class _DisplayBooksState extends State<DisplayBooks> {
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<List<Book>>(
-      future: booksAPI.fetchBooks("harry+potter"),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(snapshot.data![index].title),
-                  subtitle: Text(snapshot.data![index].author),
-                  leading: Image.network(snapshot.data![index].imageUrl),
-                );
-              });
-        } else if (snapshot.hasError) {
-          return Text("${snapshot.error}");
-        }
-        return const CircularProgressIndicator();
-      },
-    );
-  }
-}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -43,7 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const Scaffold(body: DisplayBooks()),
+      home: const Scaffold(
+          body: ResponsiveLayout(
+              mobileScreenLayout: MobileScreenLayout(),
+              webScreenLayout: WebScreenLayout())),
       debugShowCheckedModeBanner: false,
       title: 'BooksApp',
       theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.black),
---
+++ books_app/lib/responsive/mobile_screen_layout.dart
@@ -0,0 +1,11 @@
+import 'package:books_app/display_books.dart';
+import 'package:flutter/material.dart';
+
+class MobileScreenLayout extends StatelessWidget {
+  const MobileScreenLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const DisplayBooks();
+  }
+}
